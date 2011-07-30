437c790b646c48ed51f138bb43ca900ba8ced302
hamid <hamid@localhost.(none)>
2011-07-30 07:06:34 +0500

    fixing

--- app/controllers/mvp_jobs_controller.rb
+++ app/controllers/mvp_jobs_controller.rb
@@ -34,7 +34,7 @@ class MvpJobsController < ApplicationController
       new_transaction.is_fund_transfered = true
       new_transaction.save
       recurring_request.update_attribute("next_payment_time", recurring_request.next_payment_time + 1.day)
-      seller.update_attribute("recent_balance", seller.recent_balance.to_f + recurring_request.transaction.price.to_f )
+      seller.update_attribute("recent_balance", seller.recent_balance.to_f + new_transaction.price.to_f )
     end
     flash[:notice] = "Done"
     redirect_to dashboard_path(current_user)
